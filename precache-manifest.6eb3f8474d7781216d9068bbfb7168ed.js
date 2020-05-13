5043073a436865900bfa7132f822d1036c8feed0
Vipin Kumar <vipink1203@gmail.com>
2020-05-13 16:01:11 +0530

    Updates

---
+++ precache-manifest.6eb3f8474d7781216d9068bbfb7168ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1ca7a88ea0aa59ee40a33def4819db9",
+    "url": "/covid19-react/index.html"
+  },
+  {
+    "revision": "c99020f12c0090f79276",
+    "url": "/covid19-react/static/css/main.86cb1190.chunk.css"
+  },
+  {
+    "revision": "92e6dae712a9ec35011b",
+    "url": "/covid19-react/static/js/2.51384dcd.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19-react/static/js/2.51384dcd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c99020f12c0090f79276",
+    "url": "/covid19-react/static/js/main.3eeb7ad5.chunk.js"
+  },
+  {
+    "revision": "c8bd8d495a3664e97929",
+    "url": "/covid19-react/static/js/runtime-main.7bd8c676.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19-react/static/media/logo.d7265326.png"
+  }
+]);
